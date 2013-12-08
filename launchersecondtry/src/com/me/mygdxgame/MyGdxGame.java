13e5f7c866d803687fdad887a909e4d83a21ca7c
brohanster <yadav.medhi@gmail.com>
2013-12-08 11:30:29 -0500

    testing

--- launchersecondtry/src/com/me/mygdxgame/MyGdxGame.java
+++ launchersecondtry/src/com/me/mygdxgame/MyGdxGame.java
@@ -14,6 +14,7 @@ import com.badlogic.gdx.utils.TimeUtils;
 
 public class MyGdxGame implements ApplicationListener {
 	OrthographicCamera cam;
+	
 	Texture launcher;
 	Texture bullet;
 	Rectangle projectile;
